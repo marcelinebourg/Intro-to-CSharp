e2120fe5bc6b79e69155954dd9ddcab1cacaf340
Gabriel Bourg <s198007@students.aie.edu>
2019-08-19 15:02:25 -0500

    Add project files.

---
+++ my first program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output text
+
+
+            Console.WriteLine("Hello. What is your name?");
+
+            int playerScore = 0;
+            string playerName = "";
+
+            playerName = Console.ReadLine();
+
+            int add = 0;
+
+            while (playerScore < 20) 
+            //requesting input
+            {
+                Console.Write("Increment score: ");
+                add = Convert.ToInt32(Console.ReadLine());
+
+                playerScore = playerScore + add;
+                Console.Write("current Score: ");
+                Console.WriteLine(playerScore);
+            }
+
+            Console.WriteLine("Score of ");
+            Console.WriteLine(playerScore);
+            Console.WriteLine(playerScore);
+        
+          
+            
+            
+        }
+    }
+}
